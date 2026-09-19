a207a444b34c2b8bf219c044ab3ade44f5eab29d
yumi101o <10735619@mackenzista.com.br>
2026-09-19 10:37:11 +0000

    entrega Final

--- projeto.c
+++ projeto.c
@@ -1,61 +1,97 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void OrganizarArq (int A[]; int B[]){
+void imprimirPendrive(int vetor[], int tamanho) {
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d GB\n", vetor[i]);
+    }
+}
 
+void OrganizarArq(int entrada[], int tamanho, int gb, int A[], int *a, int B[], int *b) {
+    int somaA = 0, somaB = 0;
+    *a = 0;
+    *b = 0;
+
+    for (int i = 0; i < tamanho; i++) {
+        int valor = entrada[i];
+
+        if (somaA + valor <= gb && somaB + valor <= gb) {
+            if (somaA <= somaB) {
+                A[*a] = valor;
+                somaA += valor;
+                (*a)++;
+            } else {
+                B[*b] = valor;
+                somaB += valor;
+                (*b)++;
+            }
+        }
+        else if (somaA + valor <= gb) {
+            A[*a] = valor;
+            somaA += valor;
+            (*a)++;
+        }
+        else if (somaB + valor <= gb) {
+            B[*b] = valor;
+            somaB += valor;
+            (*b)++;
+        }
+        else {
+            printf("Número %d não pode ser colocado sem ultrapassar gb\n", valor);
+        }
+    }
 }
 
 int lerArquivo() {
     FILE *arquivo;
     int numeroTeste;
 
-    arquivo = fopen("/workspaces/Projeto-N1---Analise-de-algoritmos/entrada.txt", "r");
+    arquivo = fopen("entrada.txt", "r");
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
         return 1; // erro
     }
 
-    // Lendo um inteiro
     fscanf(arquivo, "%d", &numeroTeste);
 
-    for (int i = 1; i <= nuemeroTeste; i++){
+    for (int i = 0; i < numeroTeste; i++) {
         int gb;
         int numeroArquivo;
-        int soma = 0
+        int soma = 0;
+        int a, b;
 
         fscanf(arquivo, "%d", &gb);
-
-        gb = gb/2;
+        gb = gb / 2; // cada pendrive tem metade
 
         fscanf(arquivo, "%d", &numeroArquivo);
 
-        int Documentos [numeroArquivo];
-        int PenDriveA [gb];
-        int PenDriveB [gb];
+        int Documentos[numeroArquivo];
+        int PenDriveA[gb];
+        int PenDriveB[gb];
 
-        for (int j = 0; j <= numeroArquivo && soma < gb*2; j++){
+        for (int j = 0; j < numeroArquivo; j++) {
             fscanf(arquivo, "%d", &Documentos[j]);
-            soma = soma + Documentos[j];
-
-            if (soma > gb*2) {
-                printf("Impossível gravar todos os arquivos nos pendrives.\n");
-            }
+            soma += Documentos[j];
         }
 
-        if (soma < gb*2){
+        printf("%d GB\n", gb * 2); // capacidade total
 
+        if (soma > gb * 2) {
+            printf("Impossível gravar todos os arquivos nos pendrives.\n\n");
+        } else {
+            OrganizarArq(Documentos, numeroArquivo, gb, PenDriveA, &a, PenDriveB, &b);
 
+            printf("Pendrive A (%d GB)\n", gb);
+            imprimirPendrive(PenDriveA, a);
 
+            printf("\nPendrive B (%d GB)\n", gb);
+            imprimirPendrive(PenDriveB, b);
+
+            printf("\n");
         }
-
-
     }
 
     fclose(arquivo);
-
-    // Agora você pode usar os valores lidos
-    printf("Testes: %d\n", numeroTeste);
-
     return 0; // sucesso
 }
 
